a3494f967c10e331517776fb189d442bc1d1e5ca
Diego Rodriguez <diegorodriguez93@hotmail.com>
2022-04-17 08:36:45 -0300

    upgrade footer and presentation text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,18 @@
 import { useI18n } from 'next-localization';
 
+import LinkedinIcon from 'icons/linkedin.svg';
+import EmailIcon from 'icons/email.svg';
+import GithubIcon from 'icons/github.svg';
+
+const hadleRedirectToLinkedin = () =>
+  window.open('https://www.linkedin.com/in/diego-rodriguez-paiva/', '_blank');
+
+const hadleRedirectToEmail = () =>
+  window.open('mailto:diegorodriguez93@hotmail.com', '_blank');
+
+const hadleRedirectToGithub = () =>
+  window.open('https://github.com/DiegoRodriguez93', '_blank');
+
 const Footer: React.FC = () => {
   const i18n = useI18n();
 
@@ -8,23 +21,24 @@ const Footer: React.FC = () => {
       <hr className='w-full border-1 border-gray-200 dark:border-gray-800 mb-8' />
       {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
       <h3
-        onClick={() =>
-          window.open(
-            'https://www.linkedin.com/in/diego-rodriguez-paiva/',
-            '_blank',
-          )
-        }
+        onClick={hadleRedirectToLinkedin}
         onKeyDown={(ev) => {
-          if (ev.keyCode === 13) {
-            window.open(
-              'https://www.linkedin.com/in/diego-rodriguez-paiva/',
-              '_blank',
-            );
-          }
+          if (ev.keyCode === 13) hadleRedirectToLinkedin();
         }}
         className='mb-4 p-1 font-medium cursor-pointer'>
         {i18n.t('footer.contact')} <span>Diego Rodriguez</span>
       </h3>
+      <div className='flex justify-center gap-3'>
+        <button onClick={hadleRedirectToLinkedin}>
+          <LinkedinIcon width={60} height={60} />
+        </button>
+        <button onClick={hadleRedirectToEmail}>
+          <EmailIcon width={45} height={45} />
+        </button>
+        <button onClick={hadleRedirectToGithub}>
+          <GithubIcon width={50} height={50} />
+        </button>
+      </div>
     </footer>
   );
 };
